6e66af685c21c9827c138a90a8e65dbd3f7af59e
Slug <sambeekman@hotmail.com>
2023-07-21 22:50:33 +1000

    badge test

--- index.js
+++ index.js
@@ -65,6 +65,8 @@ inquirer
         let data =
             `# ${answers.title}
 
+        ![alt-text](https://img.shields.io/badge/License-${answers.license}-blue)
+
         ## Description
         
         ${answers.description}
@@ -94,7 +96,7 @@ inquirer
         
         ## Badges
         
-        ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+        [![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)]
         
         Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
         
